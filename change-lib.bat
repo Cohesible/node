203ab15b0568321fe7177f8481460bacdbcae98a
JadenSimon <simonjaden223@gmail.com>
2024-05-18 14:06:00 -0700

    Fix lib script

--- change-lib.bat
+++ change-lib.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+
 echo LIBRARY synapse.exe > synapse.def
 echo EXPORTS >> synapse.def
 for /f "skip=19 tokens=4" %%A in ('dumpbin /exports %cd%\out\Release\node.exe') do echo %%A >> synapse.def
